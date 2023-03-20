6617e1a357a61fec0843fdf616fe42d8bd27e03a
DanielaRomero <daniela.romero@epn.edu.ec>
2023-03-19 21:13:17 -0500

    Mapa

--- ProyectoAppMovilesBimestreII/app/src/main/java/com/example/proyectoappmovilesbimestreii/NuevoReporteActivity.kt
+++ ProyectoAppMovilesBimestreII/app/src/main/java/com/example/proyectoappmovilesbimestreii/NuevoReporteActivity.kt
@@ -7,5 +7,6 @@ class NuevoReporteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo_reporte)
+
     }
 }
